31d54b917dc0aba742860acec49fe950fe82db0a
lzz <455913979@qq.com>
2019-04-07 19:02:28 +0800

    第十一次提交

--- src/main/java/com/lzz/LoginController.java
+++ src/main/java/com/lzz/LoginController.java
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by 胡梦娟 on 2018/11/27.
  */
 @RestController
-@RequestMapping("login")
 public class LoginController {
 
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String homes(){
         return "hello woshizhuyan，wozaixuejisuanji!";
     }
