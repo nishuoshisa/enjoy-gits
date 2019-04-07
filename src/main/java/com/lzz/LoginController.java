e098de5a7250d43d88f05361a7f92dd3bb84a980
lzz <455913979@qq.com>
2019-04-07 15:57:18 +0800

    第五次提交

--- src/main/java/com/lzz/LoginController.java
+++ src/main/java/com/lzz/LoginController.java
@@ -1,12 +1,9 @@
 package com.lzz;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /**
  * Created by 胡梦娟 on 2018/11/27.
